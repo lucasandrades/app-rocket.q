3e4386c66ede33ce3086f1d88da1a24fdb064aca
Lucas Andrade <lucasandrade713@gmail.com>
2021-07-18 10:11:50 -0300

    fix

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,21 +5,27 @@ module.exports = {
     const db = await Database()
     const pass = req.body.password
     let roomId
-    for (var i = 0; i < 6; i++) {
-      i == 0
-        ? (roomId = Math.floor(Math.random() * 10).toString())
-        : (roomId += Math.floor(Math.random() * 10).toString())
+    let isRoom = true
+    while (isRoom) {
+      for (var i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString())
+      }
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomsExistIds.some(roomExistId => roomsExistId === roomId)
+      if (!isRoom) {
+        await db.run(`
+          INSERT INTO rooms (
+            id,
+            pass
+          ) VALUES (
+            ${parseInt(roomId)},
+            ${pass}
+          )
+        `)
+      }
     }
-
-    await db.run(`
-      INSERT INTO rooms (
-        id,
-        pass
-      ) VALUES (
-        ${parseInt(roomId)},
-        ${pass}
-      )
-    `)
 
     await db.close()
 
